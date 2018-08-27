6adf167efd0b8bb36eb71ef870993b33a70d05fd
Eric <mayeux.er@gmail.com>
2018-08-27 18:05:14 +0200

    correction Vic + changement home

---
+++ public/javascripts/assets/js/arctextCustom.js
@@ -0,0 +1,123 @@
+
+			var $rotationUp	= $('#rotationUp').hide();
+			var $rotationUp2	= $('#rotationUp2').hide();
+			var $rotationUp3	= $('#rotationUp3').hide();
+			var $rotationUp4	= $('#rotationUp4').hide();
+			var $rotationUp5	= $('#rotationUp5').hide();
+			var $rotationUp6	= $('#rotationUp6').hide();
+			
+			google.load('webfont','1');
+			
+			google.setOnLoadCallback(function() {
+				WebFont.load({
+					google		: {
+						families	: ['Montserrat','Concert One']
+					},
+					fontactive	: function(fontFamily, fontDescription) {
+						init();
+					},
+					fontinactive	: function(fontFamily, fontDescription) {
+						init();
+					}
+				});
+			});
+			
+			function init() {
+
+				$rotationUp.show().arctext({
+					radius: 400, 
+					rotate: true,
+					dir: 1, 
+					animation	: {
+					speed	: 300
+						}});
+				$rotationUp2.show().arctext({
+					radius: 400, 
+					rotate: true,
+					dir: 1, 
+					animation	: {
+					speed	: 300
+						}});
+				$rotationUp3.show().arctext({
+					radius: 400, 
+					rotate: true,
+					dir: 1, 
+					animation	: {
+					speed	: 300
+						}});
+			$rotationUp4.show().arctext({
+					radius: 400, 
+					rotate: true,
+					dir: 1, 
+					animation	: {
+					speed	: 300
+						}});
+				$rotationUp5.show().arctext({
+					radius: 400, 
+					rotate: true,
+					dir: -1, 
+					animation	: {
+					speed	: 300
+						}});
+				$rotationUp6.show().arctext({
+					radius: 400, 
+					rotate: true,
+					dir: -1, 
+					animation	: {
+					speed	: 300
+						}});
+			/*
+			/*
+
+				$headline.show().arctext({radius: 400});
+					
+				$word1.show().arctext();
+				$word2.show().arctext({radius: 148, dir: -1});
+				
+				$example1.show().arctext({radius: 300});
+				$example2.show().arctext({radius: 400, dir: -1});
+
+				$example4.show().arctext({radius: 300});
+			
+				
+				
+				
+				$('#button_set').on('click', function() {
+					$example4.arctext('set', {
+						radius		: 140,
+						dir			: -1
+					});
+					return false;
+				});
+				$('#button_anim1').on('click', function() {
+					$example4.arctext('set', {
+						radius		: 300, 
+						dir			: -1,
+						animation	: {
+							speed	: 300,
+							easing  : 'ease-out'
+						}
+					});
+					return false;
+				});
+				$('#button_anim2').on('click', function() {
+					$example4.arctext('set', {
+						radius		: 200, 
+						dir			: 1, 
+						animation	: {
+							speed	: 300
+						}
+					});
+					return false;
+				});
+				$('#button_reset').on('click', function() {
+					$example4.arctext('set', {
+						radius		: 300, 
+						dir			: 1
+					});
+					return false;
+				});
+
+				*/
+			
+			};
