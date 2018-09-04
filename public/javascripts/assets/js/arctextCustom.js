2fcba167e32ba8e5a2a72030c158f1f593bca4a8
Eric <mayeux.er@gmail.com>
2018-09-04 18:01:20 +0200

    modif visuelle eric

--- public/javascripts/assets/js/arctextCustom.js
+++ public/javascripts/assets/js/arctextCustom.js
@@ -5,13 +5,16 @@
 			var $rotationUp4	= $('#rotationUp4').hide();
 			var $rotationUp5	= $('#rotationUp5').hide();
 			var $rotationUp6	= $('#rotationUp6').hide();
+			var $rotationUp2b	= $('#rotationUp2b').hide();
+			var $rotationUp5b	= $('#rotationUp5b').hide();
+			var $rotationUp6b	= $('#rotationUp6b').hide();
 			
 			google.load('webfont','1');
 			
 			google.setOnLoadCallback(function() {
 				WebFont.load({
 					google		: {
-						families	: ['Montserrat','Concert One']
+						families	: ['Cormorant Garamond','serif']
 					},
 					fontactive	: function(fontFamily, fontDescription) {
 						init();
@@ -45,7 +48,7 @@
 					animation	: {
 					speed	: 300
 						}});
-			$rotationUp4.show().arctext({
+				$rotationUp4.show().arctext({
 					radius: 400, 
 					rotate: true,
 					dir: 1, 
@@ -60,6 +63,27 @@
 					speed	: 300
 						}});
 				$rotationUp6.show().arctext({
+					radius: 400, 
+					rotate: true,
+					dir: -1, 
+					animation	: {
+					speed	: 300
+						}});
+				$rotationUp2b.show().arctext({
+					radius: 400, 
+					rotate: true,
+					dir: 1, 
+					animation	: {
+					speed	: 300
+						}});
+				$rotationUp5b.show().arctext({
+					radius: 400, 
+					rotate: true,
+					dir: -1, 
+					animation	: {
+					speed	: 300
+						}});
+				$rotationUp6b.show().arctext({
 					radius: 400, 
 					rotate: true,
 					dir: -1, 
