e837b638a7415c2d6cc092283bd722dc6787a86d
Eric <mayeux.er@gmail.com>
2018-09-04 16:50:04 +0200

    changement Vic - Photo et Polices

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var assert = require('assert');
-
 var path = require('path');
-var mime = require('mime');
 
 
 /* GET home page. */
@@ -13,5 +10,34 @@ router.get('/', function(req, res, next) {
       layout: false});
 });
 
+router.get('/download', function(req, res){
+	
+var file = path.join(__dirname, '/upload-folder/Presentation_programme_CDZ.pdf');
+   res.download(file, function (err) {
+       if (err) {
+           console.log("Error téléchargement");
+           console.log(err);
+       } else {
+           console.log("Success téléchargement");
+       }
+   });
+
+
+});
+
+router.get('/downloadTB', function(req, res){
+	
+var file = path.join(__dirname, '/upload-folder/Presentation_programme_CDZ.pdf');
+   res.download(file, function (err) {
+       if (err) {
+           console.log("Error téléchargement");
+           console.log(err);
+       } else {
+           console.log("Success téléchargement");
+       }
+   });
+
+
+});
 
 module.exports = router;
--- routes/indexTB.js
+++ routes/indexTB.js
@@ -1,10 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var assert = require('assert');
-
-var path = require('path');
-var mime = require('mime');
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,21 +10,6 @@ router.get('/', function(req, res, next) {
   	  description: 'descritpion formation, présentation',
       layout: 'layoutTB' 
   	});
-});
-
-router.get('/download', function(req, res){
-	
-var file = path.join(__dirname, '/upload-folder/Présentation du programme de formation Centre de Zoopedagogie.pdf');
-   res.download(file, function (err) {
-       if (err) {
-           console.log("Error téléchargement");
-           console.log(err);
-       } else {
-           console.log("Success téléchargement");
-       }
-   });
-
-
 });
 
 module.exports = router;
